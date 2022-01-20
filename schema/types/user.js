7ffaf7e07025ae8958527c249181f97ecc46e6da
JuMaLaB <julien.malaboeuf@gmail.com>
2022-01-20 17:00:27 +0100

    m3-07 Reusable field definitions

--- schema/types/user.js
+++ schema/types/user.js
@@ -4,12 +4,16 @@ const {
   GraphQLString,
   GraphQLNonNull,
 } = require('graphql');
+const { fromSnakeCase } = require('../../lib/util');
 
 module.exports = new GraphQLObjectType({
   name: 'UserType',
 
   fields: {
     id: { type: GraphQLID },
+    firstName: fromSnakeCase(GraphQLString),
+    lastName: fromSnakeCase(GraphQLString),
     email: { type: new GraphQLNonNull(GraphQLString) },
+    createdAt: fromSnakeCase(GraphQLString),
   },
 });
