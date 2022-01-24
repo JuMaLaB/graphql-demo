425bd986d658718e4adbcd227ae1780948c61619
JuMaLaB <julien.malaboeuf@gmail.com>
2022-01-24 11:20:07 +0100

    m4-06 use mutation to add contest

--- schema/index.js
+++ schema/index.js
@@ -32,9 +32,22 @@ const RootQueryType = new GraphQLObjectType({
   },
 });
 
+const AddContestMutation = require('./mutations/add-contest');
+
+const RootMutationType = new GraphQLObjectType({
+  name: 'RootMutationType',
+
+  fields: () => {
+    return {
+      AddContest: AddContestMutation,
+      // AddName: AddNameMutation
+    };
+  },
+});
+
 const ncSchema = new GraphQLSchema({
   query: RootQueryType,
-  // mutation: ...
+  mutation: RootMutationType,
 });
 
 module.exports = ncSchema;
